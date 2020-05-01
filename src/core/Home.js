d182bc67f8bb78580db597d9566e13b6f496d188
Dave Cox <davedvst@gmail.com>
2020-05-01 08:42:57 -0700

    Initial commit of frontend repo reset 1

---
+++ src/core/Home.js
@@ -0,0 +1,9 @@
+import React from "react";
+import Layout from "./Layout";
+
+const Home = () => (
+  <Layout title="Home Page" description="MERN Stack React E-Commerce App">
+    Homie
+  </Layout>
+);
+export default Home;
