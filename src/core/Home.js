baaac8dfe75f9a104b2f2de9ca9dffe86ef9fd19
Dave Cox <davedvst@gmail.com>
2020-05-04 18:07:26 -0700

    Finished Filter and Search features for Products

--- src/core/Home.js
+++ src/core/Home.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import Layout from "./Layout";
 import { getProducts } from "./apiCore";
 import Card from "./Card";
+import Search from "./Search";
 
 const Home = () => {
   const [productsBySell, setProductsBySell] = useState([]);
@@ -38,23 +39,20 @@ const Home = () => {
     <Layout
       title="Home Page"
       description="MERN Stack React E-Commerce App"
-      className="container-fluid"
+      className="container"
     >
+      <Search />
       <h2 className="mb-4">New Arrivals</h2>
       <div className="row">
         {productsByArrival.map((product, i) => (
-          <div key={i} className="col-4 mb-3">
-            <Card product={product} />
-          </div>
+          <Card key={i} product={product} />
         ))}
       </div>
 
       <h2 className="mb-4">Best Sellers</h2>
       <div className="row">
         {productsBySell.map((product, i) => (
-          <div key={i} className="col-4 mb-3">
-            <Card product={product} />
-          </div>
+          <Card key={i} product={product} />
         ))}
       </div>
     </Layout>
